d30d1c01876ae171f636598a2ea4864502e154fa
akabkaplan <79549129+akabkaplan@users.noreply.github.com>
2021-03-08 23:22:09 +0300

    Update
    values in method have been changed

--- Criteria2021.java
+++ Criteria2021.java
@@ -12,46 +12,46 @@ public class Criteria2020 extends BaseMortgageCriteria {
 
     @Override
     public int getAgeMin() {
-        return 25;
+        return 18;
     }
 
     @Override
     public int getAgeMax() {
-        return 80;
+        return 75;
     }
 
     @Override
     public int getMonthlyIncomeMin() {
-        return 2000;
+        return 1750;
     }
 
     @Override
     public int getPrincipalMin() {
-        return 1500;
+        return 5000;
     }
 
     @Override
     public int getPrincipalMax() {
-        return 500000;
+        return 80000;
     }
 
     @Override
     public float getAnnualInterestMin() {
-        return 5;
+        return 3;
     }
 
     @Override
     public float getAnnualInterestMax() {
-        return 15;
+        return 12;
     }
 
     @Override
     public byte getYearsMin() {
-        return 5;
+        return 2;
     }
 
     @Override
     public byte getYearsMax() {
-        return 30;
+        return 25;
     }
 }
