fb0653577e5595b0b94aba5505a8a047c5084c09
barankaplan <59101253+barankaplan@users.noreply.github.com>
2021-03-08 23:39:39 +0300

    createCustomer2021 is added

--- CheckAvailability.java
+++ CheckAvailability.java
@@ -24,5 +24,15 @@ public class CheckAvailability  {
         ConsumerDatabase.addCustomer(customer);
         report.printAll();
     }
+    
+        public void createCustomer2021() {
+        BaseCustomerCreater customer =new Customer();
+        BaseMortgageCriteria criteria= new Criteria2021();
+        customer= new ReadConsole().ReadCustomerInfos(criteria,customer);
+        Calculable calculator=new MortgageCalculator2021(customer);
+        var report=new MortgageReport(calculator);
+        ConsumerDatabase.addCustomer(customer);
+        report.printAll();
+    }
 
 }
